b108861a01e189f1f3750949bb6db0e593b00695
Erik Carlsson <info@code301.com>
2015-02-15 00:04:07 +0100

    Init

---
+++ lib/Pod/Weaver/Section/Badges/For/Travis.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Pod::Weaver::Section::Badges::For::Travis;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Pod::Weaver::Section::Badges::For::Travis;
+
+=head1 DESCRIPTION
+
+Pod::Weaver::Section::Badges::For::Travis is ...
+
+=head1 SEE ALSO
+
+=cut
